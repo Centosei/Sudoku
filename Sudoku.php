e0ee9999a7a2730e5f5bdad48e48c17a01a024a3
Centosei <centoseisigarette@gmail.com>
2022-11-05 21:01:59 +0900

    made it a class, modified some methods

--- Sudoku.php
+++ Sudoku.php
@@ -1,5 +1,304 @@
 <?php
-$problem = [
+class Sudoku
+{
+    private $problem = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+
+    public $solved = false;
+    public $stuck = false;
+
+    private $attempt = 0;
+    private $init_array = array();
+    private $last_array = array();
+
+    public function setProblem($p_array)
+    {
+        $this->problem = $p_array;
+    }
+
+    public function is_done(bool $str_result = false)
+    {
+        if ($str_result)
+        {
+            if ($this->solved) return "It's solved!";
+            else if ($this->stuck) return "I'm stuck:(";
+            else return "It's not solved yet!";
+        }
+        else
+        {
+            if ($this->solved) return true;
+            else if ($this->stuck) return true;
+            else return false;
+        }
+    }
+ 
+    public function displayProblem()
+    {
+        if (!count($this->init_array))
+        {
+            echo "+" . str_repeat("-", 19) . "+";
+            echo "\n";
+            for ( $r = 0 ; $r < 9 ; ++$r )
+            {
+                echo "|";
+                for ( $c = 0 ; $c < 9 ; ++$c )
+                {
+                    echo " ";
+                    echo $this->problem[$r][$c];
+                }
+                echo " |";
+                echo "\n";
+            }
+            echo "+" . str_repeat("-", 19) . "+";
+            echo "\n";
+            echo " " . $this->is_done(true) . "\n";
+        }
+        else
+        {
+            echo "+" . str_repeat("-", 19) . "+" . str_repeat(" ", 20) . "+" . str_repeat("-", 19) . "+";
+            echo "\n";
+            for ( $r = 0 ; $r < 9 ; ++$r )
+            {
+                echo "|";
+                for ( $c = 0 ; $c < 9 ; ++$c )
+                {
+                    echo " ";
+                    echo $this->init_array[$r][$c];
+                }
+                echo " |";
+                if ($r === 3 ) echo str_repeat(" ", 4) . $this->is_done(true) . str_repeat(" ", 16 - strlen($this->is_done(true)));
+                else if ($r === 4) echo " " . str_repeat("-", 17) . "> ";
+                else if ($r === 5) echo str_repeat(" ", 4) . "# of Attempt:" . str_repeat(" ", 3);
+                else if ($r === 6) echo str_repeat(" ", 11 - strlen($this->attempt)) . $this->attempt . str_repeat(" ", 9);
+                else echo str_repeat(" ", 20);
+
+                echo "|";
+                for ( $c = 0 ; $c < 9 ; ++$c )
+                {
+                    echo " ";
+                    echo $this->problem[$r][$c];
+                }
+                echo " |";
+                echo "\n";
+            }
+            echo "+" . str_repeat("-", 19) . "+" . str_repeat(" ", 20) . "+" . str_repeat("-", 19) . "+";
+            echo "\n";
+        }
+    }
+
+    // not yet
+    public function solveProblem()
+    {
+        $this->init_array = $this->problem;
+        while (!$this->stuck)
+        {
+            for ( $num = 1 ; $num < 10 ; ++$num )
+            {
+                $this->attempt++;
+
+                // initial check
+                $this->checkRow($num);
+                $this->checkColumn($num);
+                $this->checkSquare($num);
+
+                // solve row
+                $this->solveRow($num);
+                $this->checkColumn($num);
+                $this->checkSquare($num);
+
+                // solve column
+                $this->solveColumn($num);
+                $this->checkRow($num);
+                $this->checkSquare($num);
+
+                // solve each block
+                $this->solveSquare($num);
+                $this->checkRow($num);
+                $this->checkColumn($num);
+
+                // clean up bools
+                $this->cleanBool($num);
+
+                // check if solved
+                $this->checkSolved();
+                if ($this->solved)
+                {
+                    break 2;
+                }
+            }
+
+            // check if stuck
+            $this->stuck = ($this->last_array === $this->problem);
+            if ($this->stuck) break;
+
+            // save the last result of the attempt
+            $this->last_array = $this->problem;
+        }
+    }
+
+    private function checkRow($num)
+    {
+        for ( $r = 0 ; $r < 9 ; ++$r )
+        {
+            if (in_array($num, $this->problem[$r], true))
+            {
+                for ( $c = 0 ; $c < 9 ; ++$c )
+                {
+                    if ($this->problem[$r][$c] === 0 )
+                        $this->problem[$r][$c] = 'F'; 
+                    else if ($this->problem[$r][$c] === 'T')
+                        $this->problem[$r][$c] = 'F';
+                }
+            }
+            else
+            {
+                for ( $c = 0 ; $c < 9 ; ++$c )
+                {
+                    if ($this->problem[$r][$c] === 0 )
+                        $this->problem[$r][$c] = 'T'; 
+                }
+            }
+        }
+    }
+
+    private function checkColumn($num)
+    {
+        for ( $c = 0 ; $c < 9 ; ++$c )
+        {
+            $column = array_column($this->problem, $c);
+            if (in_array($num, $column, true))
+            {
+                for ( $r = 0 ; $r < 9 ; ++$r )
+                {
+                    if ($this->problem[$r][$c] === 'T')
+                        $this->problem[$r][$c] = 'F';
+                }
+            }
+        }
+    }
+
+    private function checkSquare($num)
+    {
+        for ( $x = 0 ; $x < 3 ; ++$x )
+        {
+            for ( $y = 0 ; $y < 3 ; ++$y )
+            {
+                $square = [];
+                for ( $r = $x * 3 ; $r < $x * 3 + 3 ; ++$r )
+                {
+                    for ( $c = $y * 3 ; $c < $y * 3 + 3 ; ++$c )
+                    {
+                        $square[] = $this->problem[$r][$c];
+                    }
+                }
+                if ( in_array($num, $square, true))
+                {
+                    for ( $r = $x * 3 ; $r < $x * 3 + 3 ; ++$r )
+                    {
+                        for ( $c = $y * 3 ; $c < $y * 3 + 3 ; ++$c )
+                        {
+                            if ($this->problem[$r][$c] === 'T')
+                                $this->problem[$r][$c] = 'F'; 
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private function solveRow($num) 
+    {
+        for ( $r = 0 ; $r < 9 ; ++$r )
+        {
+            if ((array_key_exists('T', array_count_values($this->problem[$r]))) && (array_count_values($this->problem[$r])['T'] === 1))
+            {
+                $c = array_search('T', $this->problem[$r]);
+                $this->problem[$r][$c] = $num;
+            }
+        }
+    }
+
+    private function solveColumn($num) 
+    {
+        for ($c = 0 ; $c < 9 ; ++$c )
+        {
+            $column = array_column($this->problem, $c);
+            if ((array_key_exists('T', array_count_values($column))) && (array_count_values($column)['T'] === 1))
+            {
+                $r = array_search('T', $column);
+                $this->problem[$r][$c] = $num;
+            }
+        }
+    }
+
+
+    private function solveSquare($num) 
+    {
+        for ( $x = 0 ; $x < 3 ; ++$x )
+        {
+            for ( $y = 0 ; $y < 3 ; ++$y )
+            {
+                $square = array();
+                for ( $r = $x * 3 ; $r < $x * 3 + 3 ; ++$r )
+                {
+                    for ( $c = $y * 3 ; $c < $y * 3 + 3 ; ++$c )
+                    {
+                        $square[] = $this->problem[$r][$c];
+                    }
+                }
+                if ((array_key_exists('T', array_count_values($square))) && (array_count_values($square)['T'] === 1))
+                {
+                    $s = array_search('T', $square);
+                    $sr = intval($s / 3) + $x * 3;
+                    $sc = $s % 3 + $y * 3;
+                    $this->problem[$sr][$sc] = $num;
+                }
+            }
+        }
+    }
+
+    private function cleanBool($num)
+    {
+        for ( $r = 0 ; $r < 9 ; ++$r)
+        {
+            for ( $c = 0 ; $c < 9 ; ++$c )
+            {
+                if (($this->problem[$r][$c] === 'T' || $this->problem[$r][$c] === 'F'))
+                    $this->problem[$r][$c] = 0;
+            }
+        }
+    }
+
+    private function checkSolved()
+    {
+        $zero = 0;
+        for ( $r = 0 ; $r < 9 ; ++$r)
+        {
+            for ( $c = 0 ; $c < 9 ; ++$c )
+            {
+                if ($this->problem[$r][$c] === 0)
+                {
+                    $zero++;
+                }
+            }
+        }
+        if ($zero > 0)
+            $this->solved = false;
+        else
+            $this->solved = true;
+    }
+}
+
+$p_array = [
     [2, 0, 9, 6, 0, 1, 8, 0, 0],
     [0, 1, 0, 0, 5, 0, 0, 4, 0],
     [5, 0, 4, 3, 0, 9, 1, 0, 0],
@@ -10,244 +309,43 @@ $problem = [
     [0, 6, 0, 9, 0, 3, 0, 5, 0],
     [9, 0, 8, 4, 0, 0, 2, 0, 3],
 ];
+$sudoku = new Sudoku();
+$sudoku->setProblem($p_array);
+$sudoku->solveProblem();
+$sudoku->displayProblem();
 
-function displayProblem($problem)
-{
-    echo "+" . "---------" . "+";
-    echo "---------" . "+";
-    echo "\n";
-    for ( $r = 0 ; $r < 9 ; ++$r )
-    {
-        echo "|";
-        for ( $c = 0 ; $c < 9 ; ++$c )
-        {
-            echo " ";
-            echo $problem[$r][$c];
-        }
-        echo " |";
-        echo "\n";
-    }
-    echo "+" . "---------" . "+";
-    echo "---------" . "+";
-    echo "\n";
-}
+$p_array2 = [
+    [5, 0, 0, 0, 0, 6, 0, 4, 0],
+    [2, 0, 3, 4, 9, 0, 0, 0, 5],
+    [0, 0, 0, 0, 8, 1, 3, 0, 9],
+    [1, 0, 4, 0, 6, 0, 7, 0, 2],
+    [0, 7, 6, 0, 5, 2, 0, 9, 0],
+    [8, 0, 9, 0, 4, 0, 0, 1, 6],
+    [0, 0, 0, 0, 7, 0, 2, 0, 0],
+    [7, 3, 0, 0, 2, 8, 0, 5, 4],
+    [4, 0, 0, 9, 1, 0, 6, 0, 0],
+];
+$sudoku2 = new Sudoku();
+$sudoku2->setProblem($p_array2);
+$sudoku2->solveProblem();
+$sudoku2->displayProblem();
 
-function checkRow($problem, $num)
-{
-    for ( $r = 0 ; $r < 9 ; ++$r )
-    {
-        if (in_array($num, $problem[$r], true))
-        {
-            for ( $c = 0 ; $c < 9 ; ++$c )
-            {
-                if ($problem[$r][$c] === 0 )
-                    $problem[$r][$c] = 'F'; 
-                else if ($problem[$r][$c] === 'T')
-                    $problem[$r][$c] = 'F';
-            }
-        }
-        else
-        {
-            for ( $c = 0 ; $c < 9 ; ++$c )
-            {
-                if ($problem[$r][$c] === 0 )
-                    $problem[$r][$c] = 'T'; 
-            }
-        }
-    }
-    return $problem;
-}
+// incorrect problem
+$p_array3 = [
+    [0, 0, 8, 0, 0, 1, 0, 3, 0],
+    [0, 4, 0, 0, 3, 0, 1, 0, 5],
+    [9, 0, 0, 0, 0, 8, 0, 4, 0],
+    [0, 3, 0, 8, 0, 0, 5, 0, 2],
+    [1, 0, 0, 0, 7, 0, 0, 6, 0],
+    [0, 0, 5, 0, 2, 9, 8, 0, 4],
+    [0, 7, 0, 5, 0, 4, 0, 2, 1],
+    [3, 1, 1, 2, 0, 7, 4, 0, 0],
+    [0, 5, 0, 0, 0, 0, 6, 9, 7],
+];
+$sudoku3 = new Sudoku();
+$sudoku3->setProblem($p_array3);
+$sudoku3->solveProblem();
+$sudoku3->displayProblem();
 
-function checkColumn($problem, $num)
-{
-    for ( $c = 0 ; $c < 9 ; ++$c )
-    {
-        $column = array_column($problem, $c);
-        if (in_array($num, $column, true))
-        {
-            for ( $r = 0 ; $r < 9 ; ++$r )
-            {
-                if ($problem[$r][$c] === 'T')
-                    $problem[$r][$c] = 'F';
-            }
-        }
-    }
-    return $problem;
-}
-
-function checkSquare($problem, $num)
-{
-    for ( $x = 0 ; $x < 3 ; ++$x )
-    {
-        for ( $y = 0 ; $y < 3 ; ++$y )
-        {
-            $square = [];
-            for ( $r = $x * 3 ; $r < $x * 3 + 3 ; ++$r )
-            {
-                for ( $c = $y * 3 ; $c < $y * 3 + 3 ; ++$c )
-                {
-                    $square[] = $problem[$r][$c];
-                }
-            }
-            if ( in_array($num, $square, true))
-            {
-                for ( $r = $x * 3 ; $r < $x * 3 + 3 ; ++$r )
-                {
-                    for ( $c = $y * 3 ; $c < $y * 3 + 3 ; ++$c )
-                    {
-                        if ($problem[$r][$c] === 'T')
-                            $problem[$r][$c] = 'F'; 
-                    }
-                }
-            }
-        }
-    }
-    return $problem;
-}
-
-function solveRow($problem, $num) 
-{
-    for ( $r = 0 ; $r < 9 ; ++$r )
-    {
-        if ((array_key_exists('T', array_count_values($problem[$r]))) && (array_count_values($problem[$r])['T'] === 1))
-        {
-            $c = array_search('T', $problem[$r]);
-            $problem[$r][$c] = $num;
-        }
-    }
-    return $problem;
-}
-
-function solveColumn($problem, $num) 
-{
-    for ($c = 0 ; $c < 9 ; ++$c )
-    {
-        $column = array_column($problem, $c);
-        if ((array_key_exists('T', array_count_values($column))) && (array_count_values($column)['T'] === 1))
-        {
-            $r = array_search('T', $column);
-            $problem[$r][$c] = $num;
-        }
-    }
-    return $problem;
-}
-
-
-function solveSquare($problem, $num) 
-{
-    for ( $x = 0 ; $x < 3 ; ++$x )
-    {
-        for ( $y = 0 ; $y < 3 ; ++$y )
-        {
-            $square = array();
-            for ( $r = $x * 3 ; $r < $x * 3 + 3 ; ++$r )
-            {
-                for ( $c = $y * 3 ; $c < $y * 3 + 3 ; ++$c )
-                {
-                    $square[] = $problem[$r][$c];
-                }
-            }
-            if ((array_key_exists('T', array_count_values($square))) && (array_count_values($square)['T'] === 1))
-            {
-                $s = array_search('T', $square);
-                $sr = intval($s / 3) + $x * 3;
-                $sc = $s % 3 + $y * 3;
-                $problem[$sr][$sc] = $num;
-            }
-        }
-    }
-    return $problem;
-}
-
-function cleanBool($problem, $num)
-{
-    for ( $r = 0 ; $r < 9 ; ++$r)
-    {
-        for ( $c = 0 ; $c < 9 ; ++$c )
-        {
-            if (($problem[$r][$c] === 'T' || $problem[$r][$c] === 'F'))
-                $problem[$r][$c] = 0;
-        }
-    }
-    return $problem;
-}
-
-function checkSolved($problem)
-{
-    $zero = 0;
-    for ( $r = 0 ; $r < 9 ; ++$r)
-    {
-        for ( $c = 0 ; $c < 9 ; ++$c )
-        {
-            if ($problem[$r][$c] === 0)
-            {
-                $zero++;
-            }
-        }
-    }
-    if ($zero > 0)
-        return false;
-    else
-        return true;
-}
-
-
-$attempt = 0;
-$solved = false;
-$stuck = false;
-$last_array = array();
-while (!$stuck)
-{
-    for ( $num = 1 ; $num < 10 ; ++$num )
-    {
-        $problem = checkRow($problem, $num);
-        $problem = checkColumn($problem, $num);
-        $problem = checkSquare($problem, $num);
-        $problem = solveRow($problem, $num);
-        $problem = checkColumn($problem, $num);
-        $problem = checkSquare($problem, $num);
-        $problem = solveColumn($problem, $num);
-        $problem = checkRow($problem, $num);
-
-        $problem = checkSquare($problem, $num);
-        $problem = solveSquare($problem, $num);
-        $problem = checkRow($problem, $num);
-        $problem = solveColumn($problem, $num);
-        $problem = cleanBool($problem, $num);
-        $solved = checkSolved($problem);
-
-        $attempt++;
-        if ($solved)
-        {
-            echo "I solved!\n";
-            break 2;
-        }
-    }
-    $stuck = ($last_array === $problem);
-    if ($stuck) echo "I'm stuck!\n";
-    $last_array = $problem;
-}
-
-
-displayProblem($problem);
-var_dump($attempt);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$sudoku4 = new Sudoku();
+$sudoku4->displayProblem();
