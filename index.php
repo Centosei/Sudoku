a13e11e956b3df64fabaf06d2ffae1c9d6f8dd9b
Centosei <centoseisigarette@gmail.com>
2022-11-06 11:58:37 +0900

    forgot to erase debug stuff

--- index.php
+++ index.php
@@ -2,36 +2,11 @@
 require_once 'Sudoku.php';
 
 $sudoku = new Sudoku();
-
-/* var_dump($sudoku->is_done(true)); */
-$p_array = [
-    [2, 0, 9, 6, 0, 1, 8, 0, 0],
-    [0, 1, 0, 0, 5, 0, 0, 4, 0],
-    [5, 0, 4, 3, 0, 9, 1, 0, 0],
-    [1, 0, 5, 0, 3, 0, 0, 0, 9],
-    [8, 4, 0, 0, 0, 2, 0, 7, 0],
-    [3, 0, 7, 1, 8, 0, 5, 2, 0],
-    [7, 0, 2, 0, 1, 0, 4, 0, 6],
-    [0, 6, 0, 9, 0, 3, 0, 5, 0],
-    [9, 0, 8, 4, 0, 0, 2, 0, 3],
-];
-/* var_dump($_POST); */
-/* print_r($sudoku->problem); */
 if (!empty($_POST))
 {
     $sudoku->importProblem($_POST);
-    /* echo "<pre>"; */
-    /* var_dump($sudoku->problem); */
-    /* echo "</pre>"; */
 }
-/* else */
-/* { */
-/*     $sudoku->setProblem($p_array); */
-/* } */
-/* print_r($sudoku->problem); */
 $sudoku->solveProblem();
-/* var_dump($sudoku->is_done(true)); */
-/* $sudoku->displayProblem(); */
 ?>
 <!DOCTYPE html>
 <html lang="en">
